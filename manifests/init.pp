d5d7ac49424f24c6df28893b439084ba278eddad
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-03-26 12:56:38 +0900

    bump up to 1.5.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,14 +7,14 @@ class vagrant {
 
 class vagrant::centos {
   exec { vagrant:
-    command => "rpm -Uvh https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.0_x86_64.rpm",
+    command => "rpm -Uvh https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.1_x86_64.rpm",
     creates => "/usr/bin/vagrant",
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
   }
 }
 
 class vagrant::darwin {
-  $version = "1.5.0"
+  $version = "1.5.1"
 
   package { "Vagrant-${version}.dmg":
     ensure   => installed,
